36a3b677390980459b3bad79e2fc06d66da77f47
seiichi <poipoipoi33378@gmail.com>
2021-09-10 13:37:21 +0900

    init

---
+++ zoo/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"zoo/animals"
+)
+
+func main()  {
+	fmt.Println(animals.ElephantFeed())
+	fmt.Println(animals.RabbitFeed())
+	fmt.Println(animals.MonkeyFeed())
+}
